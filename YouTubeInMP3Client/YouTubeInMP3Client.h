8b7fda57ef11ccb6c0e70dd7cc94f7983e0e7619
Miles Hollingsworth <mhmiles@users.noreply.github.com>
2016-05-11 13:53:18 -0700

    Initial Commit

---
+++ YouTubeInMP3Client/YouTubeInMP3Client.h
@@ -0,0 +1,19 @@
+//
+//  YouTubeInMP3Client.h
+//  YouTubeInMP3Client
+//
+//  Created by Miles Hollingsworth on 5/11/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YouTubeInMP3Client.
+FOUNDATION_EXPORT double YouTubeInMP3ClientVersionNumber;
+
+//! Project version string for YouTubeInMP3Client.
+FOUNDATION_EXPORT const unsigned char YouTubeInMP3ClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouTubeInMP3Client/PublicHeader.h>
+
+
